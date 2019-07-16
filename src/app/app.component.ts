6d577e57f7740ac7f9d0b59c4073044a91056146
ImsirovicAjdin <ajdika@live.com>
2019-07-16 14:10:44 +0200

    What Subject will allow me to do is call .next every time I click the button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,28 @@ import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'my-app',
   template: `
-    <button (click)="onClick()">Update</button>               <!-- (2) -->
+    <button (click)="click$.next()">Update</button>
     <h1>{{clock | async | date: 'MMM d, y, h:mm:ss a'}}</h1>
   `
 })
 export class AppComponent  {
-  onClick() {
-    
-  }
-  
-  clock = Observable
-      .interval(1000)
-      .map(()=> new Date());
+  /*
+  To resolve this for now, I'll create something called a 'Subject', and this will be a new Subject. 
+  What Subject will allow me to do is call .next every time I click the button.
+  And what .next is gonna do is basically push the stream forward 1 tick.
+  So if I just take out all of this clock stuff, which was just going every second before, and now I say, this.clock is this.click$, and I'm gonna map those clicks to a new Date.
+  */
+  click$ = new Subject();
 
-  // Make Observable from event, then find the event and pass in the name you wanna listen to;
-  // So we have these 2 conflicting approaches where we want to find an event (1), or handle event in HTML (2):
+  clock;
+
   constructor(){
-    Observable.fromEvent(, 'click') // (1)
+    this.clock = this.click$.map(()=> new Date());  
   }
 
 }
