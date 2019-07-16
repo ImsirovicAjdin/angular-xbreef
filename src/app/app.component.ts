81fbd5aede7fecf4fb8c94a7ded0a78ba63be554
ImsirovicAjdin <ajdika@live.com>
2019-07-16 14:04:24 +0200

    We have 2 conflicting approaches: handling events in HTML or thru Observable.fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// Hande click events with Subjects
+
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
@@ -6,11 +8,23 @@ import 'rxjs/add/operator/map';
 @Component({
   selector: 'my-app',
   template: `
+    <button (click)="onClick()">Update</button>               <!-- (2) -->
     <h1>{{clock | async | date: 'MMM d, y, h:mm:ss a'}}</h1>
   `
 })
 export class AppComponent  {
+  onClick() {
+    
+  }
+  
   clock = Observable
       .interval(1000)
       .map(()=> new Date());
+
+  // Make Observable from event, then find the event and pass in the name you wanna listen to;
+  // So we have these 2 conflicting approaches where we want to find an event (1), or handle event in HTML (2):
+  constructor(){
+    Observable.fromEvent(, 'click') // (1)
+  }
+
 }
