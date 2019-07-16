e83020659ffc41c3712a3ae4fe09ce3d84a6aa7f
ImsirovicAjdin <ajdika@live.com>
2019-07-16 14:28:56 +0200

    Remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// 4. Handle clicks and Intervals Together with Merge
+
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
@@ -17,19 +19,9 @@ export class AppComponent  {
   clock;
 
   constructor(){
-    /* (3) So we'll say this.click$, because Subject is a type of observable, and we'll say Observable.interval; so we're saying, 'let either one of these be able to map to a new Date'
-    Observable.merge(
-        this.click$,
-        Observable.interval(1000)
-    ).map(()=> new Date());
-    */
-
-    // (4) and then I can take my clock and assign that to that new stream of merge
     this.clock = Observable.merge(
         this.click$,
-        Observable.interval(5000) // (5) and to show this off I'll change the interval to 5s
+        Observable.interval(5000)
     ).map(()=> new Date());
   }
-  // (6) so we have those 2 streams working for us, whenever I click -- (this.click$) --, or
-  // whenever 5s passes -- (Observable.interval(5000)) -- my clock will now update
 }
